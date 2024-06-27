34f72ebde9e5b84e460793737b5263e0d1ceed36
Manahil rasheed <manahilrasheed507@gmail.com>
2024-06-27 03:36:41 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,171 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+
+
+// Bank account interface
+
+interface Bankaccount{
+    accountNumber :number;
+    balance: number;
+    Withdraw(amount: number): void
+    deposit(amount: number): void
+    checkBalance(): void
+
+}
+
+
+// Bank account 
+class Bankaccount implements Bankaccount{
+    accountNumber: number;
+    balance: number;  
+
+    constructor(accountNumber: number, balance: number){
+        this.accountNumber = accountNumber;
+        this.balance = balance
+    }
+    
+  // Debit money
+  Withdraw(amount: number): void {
+      if(this.balance >= amount){
+        this.balance -= amount;
+        console.log(`Withdrawal of ${amount} sucessful, Remaining balance; ${this.balance} `);
+      }else {
+        console.log("Insufficient balance.")
+      }
+  } 
+
+
+  // Credit mpney 
+  deposit(amount: number): void {
+      if(amount > 100){
+        amount -= 1;  // $1 fee charged if more than $100 is deposit 
+      } this.balance += amount;
+      console.log(`Deposit of ${amount} sucessfull, Remaining balance ${this.balance}`)
+  }
+
+
+// Check balance 
+checkBalance(): void {
+    console.log(`current balance: ${this.balance}`);
+
+}
+
+
+}
+
+
+// Customer class 
+
+ class customer{
+    firstName: string;
+    LastName: string;
+    gender: string;
+    age: number;
+    mobileNumber: number;
+    account: Bankaccount;
+
+     
+    constructor(firstName: string, LastName: string, gender: string, age: number, mobileNumber: number, account: Bankaccount){
+        this.firstName = firstName
+        this.LastName = LastName
+        this.gender = gender
+        this.age = age
+        this.mobileNumber = mobileNumber
+        this.account = account
+    }
+
+ }
+
+
+
+// Create bank account
+
+const accounts: Bankaccount[] = [
+    new Bankaccount (1001, 500),
+    new Bankaccount (1002, 1000),
+    new Bankaccount (1003, 2000),
+
+]
+
+
+// Create customers 
+
+
+const customers: customer[] = [
+    new customer ("Hamza","Khan","Male",35,3343045316, accounts[0]),
+    new customer ("Syedashanzay","Khan","female",24,3362353818, accounts[1]),
+    new customer ("Ayesha","Khan","female",35,3341045316, accounts[2])
+]
+
+
+// function to interact with bank account 
+
+async function service() {
+    do {
+        const  accountNumberInput = await inquirer.prompt({
+            name: "accountnumber",
+            type: "number",
+            message: "Enter your account number:"
+        })
+
+
+        const customer = customers.find(customer => customer.account.accountNumber === accountNumberInput.accountnumber)
+        if(customer){
+            console.log(`Welcome, ${customer.firstName} ${customer.LastName} !\n`)
+            const ans = await inquirer.prompt([{
+                name:"select",
+                type:"list",
+                message: "Select and operration",
+                choices: ["Deposit","Withdraw","Check balance","Exit"]
+            }])
+
+            
+
+            switch (ans.select) {
+                case "Deposit":
+                    const depositamount = await inquirer.prompt({
+                        name:"amount",
+                        type:"number",
+                        message:"Enter the amount to deposit:"
+                    })
+
+                     customer.account.deposit(depositamount.amount)
+                     break;
+                case "Withdraw":
+                        const Withdrawamount = await inquirer.prompt({
+                            name:"amount",
+                            type:"number",
+                            message:"Enter the amount to Withdraw:"
+                        })
+    
+                         customer.account.Withdraw(Withdrawamount.amount)
+                         break;
+
+                         case "Check balance":
+                            customer.account.checkBalance();
+                            break;
+                        case "Exit":
+                            console.log("Existing bank program...")
+                            console.log("\n Thank you for using our bank services. Have a great day!")
+                            return;
+            }
+
+
+        }else {
+            console.log("Invalid account number. Plaese try again.")
+        }
+
+
+
+
+
+    } while(true)
+
+
+        
+}
+
+service()
